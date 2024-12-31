f6c2e0139c2fc47d04dec65d503fdcd60f46f332
RegiIgnis <RegiIgnis@users.noreply.github.com>
2024-12-31 02:46:10 -0500

    Add project files.

---
+++ IntroTutorial.cs
@@ -0,0 +1,54 @@
+﻿using RedLoader;
+using SonsSdk;
+using static RedLoader.RLog;
+
+namespace IntroTutorial;
+
+public class IntroTutorial : SonsMod
+{
+    public IntroTutorial()
+    {
+
+        // Uncomment any of these if you need a method to run on a specific update loop.
+
+        //OnUpdateCallback = OnWorldUpdate;
+        //OnLateUpdateCallback = MyLateUpdateMethod;
+        //OnFixedUpdateCallback = MyFixedUpdateMethod;
+        //OnGUICallback = MyGUIMethod;
+
+        HarmonyPatchAll = true;
+        // Uncomment this to automatically apply harmony patches in your assembly.
+    }
+
+    protected override void OnInitializeMod()
+    {
+        // Do your early mod initialization which doesn't involve game or sdk references here
+        Config.Init();
+    }
+
+    protected override void OnSdkInitialized()
+    {
+        // Do your mod initialization which involves game or sdk references here
+        // This is for stuff like UI creation, event registration etc.
+        IntroTutorialUi.Create();
+        //SettingsRegistry.CreateSettings(this, null, typeof(Config));
+    }
+
+    protected override void OnGameStart()
+    {
+        // This is called once the player spawns in the world and gains control.
+        Msg("MyMessage");
+        Msg("John");
+        SonsTools.ShowMessage("Test",10.5f);
+        Msg("");
+        Warning("Warning Test");
+        Error("Error Test");
+        WriteLine(11);
+        MsgDirect(System.Drawing.Color.Green, "MsgDirect Test");
+        BigError("", "BigError");
+        Debug("Debug");
+        SonsTools.ShowMessage("SonsTools Show");
+    }
+}
+
+// Press F9 to open the console in game. Thats where the message testing testing will appear.
---
+++ IntroTutorialUi.cs
@@ -0,0 +1,11 @@
+namespace IntroTutorial;
+
+using static SUI.SUI;
+
+public class IntroTutorialUi
+{
+    public static void Create()
+    {
+        // Ui creation code goes here
+    }
+}
